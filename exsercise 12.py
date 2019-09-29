a6a4c83a32f20c99447fcdc8ffabe8f26b689511
alha0276 <alha0276@algonquinlive.com>
2019-09-29 16:36:29 -0400

    first commit

---
+++ exsercise 12.py
@@ -0,0 +1,9 @@
+#   this Python Program to convert  from temperature in celsius to fahrenheit 
+# Input  by the user in degree celsius
+ 
+# take input from the user
+celsius = float(input('Enter degree Celsius: '))
+ 
+# the eqation infahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
